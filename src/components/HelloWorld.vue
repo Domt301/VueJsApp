fcd2799903608de00e738429d7c5dbec11619475
Leonardo Trimarchi <Leonardod.Trimarchi@gmail.com>
2018-10-01 06:30:58 -0400

    minor changes

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,6 +2,9 @@
   <div class="hello">
     <h1>{{ msg }}</h1>
     <p>
+      I created this vueJs app using the Vue cli. I then created a droplet in digital ocean and deployed there!
+    </p>
+    <p>
       For guide and recipes on how to configure / customize this project,<br>
       check out the
       <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
